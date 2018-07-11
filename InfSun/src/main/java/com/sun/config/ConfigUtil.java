75c846509cb1cd762b699232d19373b7be41e3d4
zhuoyue9527 <zhuoyue9527@163.com>
2018-07-11 20:46:06 +0800

    票据状态 常量

--- InfSun/src/main/java/com/sun/config/ConfigUtil.java
+++ InfSun/src/main/java/com/sun/config/ConfigUtil.java
@@ -2,29 +2,31 @@ package com.sun.config;
 
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
+
 /**
  * 读取application.conf 配置文件,application.conf需要在classpath路径下面
+ * 
  * @author sunning
  *
  */
 public class ConfigUtil {
-	
-	private static final String zoneCode ="zone_code";
-	private static final String url ="url";
 
-	public  static final String version="1.0";
-	public  static final String paylistfmt="01";
-	public  static final String unit="个";
-	
-	public  static final String stdtype="无限制";
-	public  static final String standard = "0.0-0.0";
-	
-	public  static final String unpaid = "1"; //未缴款
-	public  static final String paid = "2";  //已缴款
-	public  static final String abandoned = "3";  //已作废
-	
-	public  static final String delay_amount = "0";  //滞纳金
-	
+	private static final String zoneCode = "zone_code";
+	private static final String url = "url";
+
+	public static final String version = "1.0";
+	public static final String paylistfmt = "01";
+	public static final String unit = "个";
+
+	public static final String stdtype = "无限制";
+	public static final String standard = "0.0-0.0";
+
+	public static final String printed = "1"; // 已打印
+	public static final String cancel = "2"; // 已作废
+	public static final String paid = "3"; // 已缴款
+
+	public static final String delay_amount = "0"; // 滞纳金
+
 	private static Config conf;
 
 	static {
@@ -34,18 +36,23 @@ public class ConfigUtil {
 	public static String getStringConfigProperties(String name) {
 		return conf.getString(name);
 	}
+
 	public static int getIntConfigProperties(String name) {
 		return conf.getInt(name);
 	}
+
 	/**
-	 *  获取非税节点的行政区划分
+	 * 获取非税节点的行政区划分
+	 * 
 	 * @return
 	 */
 	public static String getZoneCode() {
 		return getStringConfigProperties(zoneCode);
 	}
+
 	/**
 	 * 获取支付平台URL
+	 * 
 	 * @return
 	 */
 	public static String getUrl() {
